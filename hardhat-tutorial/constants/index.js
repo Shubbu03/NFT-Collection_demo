739040ed36cb1c583d2dbb2f9878eb7e50b3d961
Shubbu03 <thatcoderguyshubham@gmail.com>
2023-03-12 10:18:06 +0530

    this is a basic app to mint nfts an allow the whitelist user to start presale

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x92Bc0b20955d8Fca4cdDF70c925b4e02cAf9bF1A";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
